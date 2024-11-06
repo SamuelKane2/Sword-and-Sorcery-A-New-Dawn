909074025215529f06ff2aad39d57d35eeca5fa9
BrendanFisher09 <bfisher@ftstudent.org>
2024-11-06 12:28:18 -0500

    :)

---
+++ difficulty.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _on_easy_pressed():
+	pass # Replace with function body.
+
+
+func _on_medium_pressed():
+	pass # Replace with function body.
+
+
+func _on_hard_pressed():
+	pass # Replace with function body.
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Menu.tscn")
