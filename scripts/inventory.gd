f4380e83f75f54db794f7fa87f2930a95ffa2e94
SamuelKane2 <slkane3@icloud.com>
2024-11-11 13:16:21 -0500

    Hmmm

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -1,5 +1,19 @@
 extends Control
 
+@onready var inventory: Control = %Inventory
+
+func _ready():
+	inventory.close()
+
+func _input(event):
+	print("Event")
+	if event.is_action_pressed("toggle_inventory"):
+		print("E")
+		if inventory.isOpen:
+			inventory.close()
+		else: 
+			inventory.open()
+
 var isOpen: bool = false
 
 func open():
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,13 +1,1 @@
 extends Control
-
-@onready var inventory: Control = %Inventory
-
-func _ready():
-	inventory.close()
-
-func _input(event):
-	if event.is_action_pressed("toggle_inventory"):
-		if inventory.isOpen:
-			inventory.close()
-		else: 
-			inventory.open()
