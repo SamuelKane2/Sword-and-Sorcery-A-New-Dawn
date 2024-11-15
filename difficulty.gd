04782e29d6a2ddb16e49e15c4547945ad9e575f3
BrendanFisher09 <bfisher@ftstudent.org>
2024-11-15 11:59:13 -0500

    h

--- difficulty.gd
+++ difficulty.gd
@@ -3,15 +3,15 @@ extends Control
 
 
 func _on_easy_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
 func _on_medium_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
 func _on_hard_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
 func _on_back_pressed():
