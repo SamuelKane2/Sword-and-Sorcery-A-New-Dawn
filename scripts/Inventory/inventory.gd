32f9cf5c7cdbd7503c30560bbef3a4ed55596ec0
SamuelKane2 <slkane3@icloud.com>
2024-11-16 08:28:09 -0500

    Trying to make the inventory work

--- scripts/Inventory/inventory.gd
+++ scripts/Inventory/inventory.gd
@@ -1,9 +1,16 @@
 extends Control
 
 @onready var inventory: Control = %Inventory
+@onready var Inventory: Inventory = preload("res://scenes/Inventory/Inventory Items/playerInventory.tres")
+@onready var main_inventory: GridContainer = $Inventory/MainInventory
 
 func _ready():
 	inventory.close()
+	update()
+
+func update():
+	for i in range(min(inventory.items.size(), main_inventory.get_children().size())):
+		main_inventory.get_children()[i].update(inventory.items[i])
 
 func _input(event):
 	if event.is_action_pressed("toggle_inventory"):
