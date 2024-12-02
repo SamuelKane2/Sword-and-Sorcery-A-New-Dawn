59476306634644ba7003d110e4462df797ce3110
SamuelKane2 <slkane3@icloud.com>
2024-12-02 13:15:11 -0500

    Trying to make it work

--- scripts/Inventory/collectable.gd
+++ scripts/Inventory/collectable.gd
@@ -2,5 +2,7 @@ extends Area2D
 
 @export var itemRes: InventoryItem
 
-func _on_body_entered(body: Node2D) -> void:
+func collect(inventory: Inventory):
+	inventory.insert(itemRes)
 	queue_free()
+	
--- scripts/Inventory/inventory_cell.gd
+++ scripts/Inventory/inventory_cell.gd
@@ -2,6 +2,7 @@ extends Button
 
 @onready var background_sprite: Sprite2D = $backgroundSprite
 @onready var center_container: CenterContainer = $CenterContainer
+@onready var item_sprite: Sprite2D = $itemSprite
 
 var itemStackGui: ItemStack
 
@@ -9,10 +10,10 @@ func insert(isg: ItemStack):
 	itemStackGui = isg
 	center_container.add_child(itemStackGui)
 
-#func update(item: InventoryItem):
-#	if !item:
-#		item_sprite.visible = false
-#	else:
-#		item_sprite.visible = true
-#		item_sprite.texture = item.texture
-							 #slot.item.texture
+func update(item: InventoryItem):
+	if !item:
+		item_sprite.visible = false
+	else:
+		item_sprite.visible = true
+		item_sprite.texture = item.texture
+		#slot.item.texture
