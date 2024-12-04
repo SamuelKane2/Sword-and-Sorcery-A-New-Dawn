a9c567d510662dd17a23e8942b4c58fa5f5dc691
JoshuaxLasley <jlasley@ftstudent.org>
2024-12-04 13:00:41 -0500

    Actually sets up the health

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,4 @@
-class_name Health
-extends Node
-
-
+extends Node2D
 const MAX_HEALTH = 20
 var health = MAX_HEALTH
 
@@ -12,16 +9,14 @@ func update_health_ui():
 	set_health_label()
 
 func set_health_label() -> void:
-	$HealthLabel.text = "Health: %" % health 
-	##should allow the UI and this code to be set up toghther
-	##Should allow for the UI to change when damage is taken
+	$HealthLabel.text = "Health: %" % health
 
 func _input(event: InputEvent) -> void: #need to change this to where it will recive actual damage
 	if event.is_action_pressed("ui_accept"):
 		damage()
 	
-func damage() -> void: 
-	health -= 1 #makes it so you take damage
-	if health < 0: 
+func damage() -> void:
+	health -= 1
+	if health < 0:
 		health = MAX_HEALTH
-	update_health_ui() #updates the UI for what your health is
+	update_health_ui()
