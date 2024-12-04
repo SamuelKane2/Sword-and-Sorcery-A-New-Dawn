ad11e9bc9e0fb0175850c83c6c09e0b93051a80f
JoshuaxLasley <jlasley@ftstudent.org>
2024-12-04 13:02:07 -0500

    Boom

--- Player/player.gd
+++ Player/player.gd
@@ -1,4 +1,5 @@
 extends Node2D
+
 const MAX_HEALTH = 20
 var health = MAX_HEALTH
 
@@ -9,14 +10,16 @@ func update_health_ui():
 	set_health_label()
 
 func set_health_label() -> void:
-	$HealthLabel.text = "Health: %" % health
+	$HealthLabel.text = "Health: %" % health 
+	##should allow the UI and this code to be set up toghther
+	##Should allow for the UI to change when damage is taken
 
 func _input(event: InputEvent) -> void: #need to change this to where it will recive actual damage
 	if event.is_action_pressed("ui_accept"):
 		damage()
 	
-func damage() -> void:
-	health -= 1
-	if health < 0:
+func damage() -> void: 
+	health -= 1 #makes it so you take damage
+	if health < 0: 
 		health = MAX_HEALTH
-	update_health_ui()
+	update_health_ui() #updates the UI for what your health is
