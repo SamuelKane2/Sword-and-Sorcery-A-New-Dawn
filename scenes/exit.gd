db342fe5af2b29e691e5dbb41b1cc4326e4a79bd
OliverTheLittleLion <bjohnson5@ftstudent.org>
2024-12-06 12:53:25 -0500

    hi

--- scenes/exit.gd
+++ scenes/exit.gd
@@ -1,4 +1,14 @@
 extends Area2D
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+var game_scene_path : String = "res://scenes/game.tsccn"
+
+func _ready():
+	self.body_entered.connect(_on_body_entered)
+	add_to_group("player")
+	
+func _on_body_entered(body : Node) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene("res://scenes/game.tscn")
+
+func _on_area_entered(area: Area2D):
+	get_tree().change_scene("res://target_scene.tscn")
--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -2,7 +2,8 @@ extends Control
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/Difficulty.tscn")
-
+	print("start")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+	print("quit")
