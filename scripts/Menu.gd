290f751835d6ef20976fe1802cf2ac837e3d5b45
OliverTheLittleLion <bjohnson5@ftstudent.org>
2024-12-10 12:37:59 -0500

    hd

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/Difficulty.tscn")
-	print("start")
+	print("play")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
